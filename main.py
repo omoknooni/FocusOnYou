9a84f35ea1f8fee9d8a42186355dbe7eaf0c64ca
omoknooni <hihm0609@gmail.com>
2023-12-06 22:01:27 +0900

    feat: simple login

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, File, UploadFile, Request, Form
+from fastapi import FastAPI, File, UploadFile, Request, Response, Form, Depends
 from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordBearer
 from botocore.exceptions import ClientError
 from datetime import datetime
 from dotenv import load_dotenv
@@ -11,6 +12,9 @@ import boto3
 import os, sys
 import uuid
 import threading
+from datetime import timedelta
+from jose import jwt, JWTError
+from fastapi import HTTPException
 
 load_dotenv()
 
@@ -23,6 +27,13 @@ S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 dynamo = boto3.client('dynamodb', aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),aws_secret_access_key=os.getenv("AWS_SECRET_KEY"), region_name=os.getenv('AWS_REGION'))
 TABLE_NAME = os.getenv("TABLE_NAME")
+
+DMO_USERNAME = os.getenv("DMO_USERNAME")
+DMO_PASS = os.getenv("DMO_PASS")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 class ProgressPercentage(object):
     def __init__(self, filename, size):
         self._filename = filename
@@ -57,19 +68,82 @@ def allowed_file(filename):
     else:
         return False
 
+def create_access_token(data: dict, expires_delta: int):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def auth_token(response: Response, username: str, password: str):
+    if username == DMO_USERNAME and password == DMO_PASS:
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": username}, expires_delta=access_token_expires.total_seconds()
+        )
+
+        response.set_cookie(key="access_token", value=access_token, httponly=True, expires=access_token_expires)
+        return True
+    else:
+        return False
+
+async def get_user(request: Request):
+    token = request.cookies.get("access_token")
+    if token is None:
+        return None
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            return None
+        return {"username": username}
+    except JWTError:
+        raise credentials_exception
 
 @app.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/upload", response_class=HTMLResponse)
-async def upload(request: Request):
-    return templates.TemplateResponse("upload.html", {"request": request})
+@app.get("/login")
+def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
-## TODO : 업로드 시 progress bar 추가
+@app.post("/login")
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    try:
+        response = RedirectResponse(url="/upload", status_code=302)
+        validation = await auth_token(response, username, password)
+
+        if not validation:
+            return templates.TemplateResponse("login.html", {"request": request, "msg": "Invalid credential"})
+        return response
+    except HTTPException:
+        return templates.TemplateResponse("login.html", {"request": request, "msg": "Invalid credential"})
+
+@app.get("/logout")
+async def logout(request: Request):
+    response = templates.TemplateResponse("login.html", {"request": request, "msg": "logout Success"})
+    response.delete_cookie(key="access_token")
+    return response
+
+@app.get("/upload")
+async def upload(request: Request):
+    user = await get_user(request)
+    if user is None:
+        return RedirectResponse("/login", status_code=302)
+    return templates.TemplateResponse("upload.html", {"request": request, "user": user})
+
 @app.post("/upload")
-async def upload(face_name: str = Form(...), face_image: UploadFile = File(...), target_video: UploadFile = File(...)):
-    
+async def upload(request: Request, face_name: str = Form(...), face_image: UploadFile = File(...), target_video: UploadFile = File(...)):
+    user = await get_user(request)
+    if user is None:
+        return RedirectResponse("/login", status_code=302)
+
     # file extension validation logic
     if not allowed_file(face_image.filename) or not allowed_file(target_video.filename):
         return JSONResponse(status_code=400, content={"message": "Invalid file extension"})
@@ -120,6 +194,10 @@ async def upload(face_name: str = Form(...), face_image: UploadFile = File(...),
     
 @app.get("/jobs")
 async def list_jobs(request: Request):
+    user = await get_user(request)
+    if user is None:
+        return RedirectResponse("/login", status_code=302)
+    
     # Retrieve primary key of dynamodb table
     try:
         db_response = dynamo.scan(
@@ -130,12 +208,16 @@ async def list_jobs(request: Request):
         count = db_response['Count']
         jobs = db_response['Items']
         jobs = [{'job_id': job['job_id']['S'], 'uploaded_at': job['uploaded_at']['S']} for job in jobs]
-        return templates.TemplateResponse("job_list.html", {"request": request, "jobs": jobs, "count": count})
+        return templates.TemplateResponse("job_list.html", {"request": request, "jobs": jobs, "count": count, "user": user})
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": str(e)})
 
 @app.get("/jobs/{job_id}")
 async def read_job(request: Request, job_id: str):
+    user = await get_user(request)
+    if user is None:
+        return RedirectResponse("/login", status_code=302)
+    
     try:
         db_response = dynamo.get_item(
             TableName=TABLE_NAME,
@@ -147,4 +229,4 @@ async def read_job(request: Request, job_id: str):
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": str(e)})
     # return JSONResponse(content={"result": "Complete", "job_id": job_id, "Object": job_obj})
-    return templates.TemplateResponse("job_detail.html", {"request": request, "job_id": job_id, "job_obj": job_obj})
+    return templates.TemplateResponse("job_detail.html", {"request": request, "job_id": job_id, "job_obj": job_obj, "user": user})
