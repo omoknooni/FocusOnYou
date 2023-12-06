6043cfd76f9169dc905b90b0a0845fb1e428eedc
omoknooni <hihm0609@gmail.com>
2023-12-06 17:43:48 +0900

    fix: add transcoded_video URL link

--- job_result.py
+++ job_result.py
@@ -12,6 +12,7 @@ logger.setLevel(logging.INFO)
 dynamo = boto3.client('dynamodb')
 
 FOCUSONYOU_RESULT_BUCKET = os.environ['FOCUSONYOU_RESULT_BUCKET']
+BUCKET_REGION=os.environ['BUCKET_REGION']
 TABLE_NAME = os.environ['TABLE_NAME']
 SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
 HOOK_URL = os.environ['HOOK_URL']
@@ -26,20 +27,23 @@ def lambda_handler(event, context):
     job_id = message.get('input').get('key').split('/')[1]
     output_filename = message.get('outputs')[0].get('key')
 
+    transcoded_video_url = f"https://{FOCUSONYOU_RESULT_BUCKET}.s3.{BUCKET_REGION}.amazonaws.com/{output_filename}"
+
     if transjob_status == 'COMPLETED':
         dynamo.update_item(
             TableName=TABLE_NAME,  
             Key={
                 'job_id': {'S': job_id}
             },
-            UpdateExpression="set job_status = :s",
+            UpdateExpression="set job_status = :s, transcoded_video = :t",
             ExpressionAttributeValues={
                 ':s': {'S': 'TRANSCODE_COMPLETE'},
+                ':t': {'S': transcoded_video_url}
             }
         )
         slack_message = {
             'channel': SLACK_CHANNEL,
-            'text': f"*[FocusOnYou]*\n\nJob {job_id} has been completed!\nVideo : {'s3://'+FOCUSONYOU_RESULT_BUCKET+'/'+output_filename}"
+            'text': f"*[FocusOnYou]*\n\nJob {job_id} has been completed!\nVideo : {transcoded_video_url}"
         } 
         
         req = Request(HOOK_URL, json.dumps(slack_message).encode('utf-8'))
