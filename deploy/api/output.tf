d688414eab2a16070a96f49d659ee04372bf4501
omoknooni <hihm0609@gmail.com>
2025-05-24 14:47:31 +0900

    fix: use api_endpoint(using of default stage)

--- deploy/api/output.tf
+++ deploy/api/output.tf
@@ -1,4 +1,4 @@
 output "backend_api_domain" {
-  value = aws_apigatewayv2_stage.staging.invoke_url
+  value = aws_apigatewayv2_api.http_api.api_endpoint
   description = "The API Gateway domain, Use this value with build frontend (.env REACT_APP_API_URL)"
 }
